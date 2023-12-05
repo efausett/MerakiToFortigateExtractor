35a1f81419e3714d617416a67e65f6b5ab32b3f4
devnetkid <jessefausett@gmail.com>
2023-12-05 00:05:44 -0700

    Changed to print one large file with all settings

--- vlans.py
+++ vlans.py
@@ -18,7 +18,7 @@ def format_fixed_addresses(vlan_name, fixed_data):
         fixed.append(f'    next\n')
     fixed.append("end\n")
     fixed_file = vlan_name + "_fixed"
-    fileops.writelines_to_file(fixed_file, fixed)
+    return fixed
 
 
 def format_reserved_addresses(vlan_name, reserved_data):
@@ -33,11 +33,13 @@ def format_reserved_addresses(vlan_name, reserved_data):
         reserved.append(f'    next\n')
     reserved.append("end\n")
     reserved_file = vlan_name + "_reserved"
-    fileops.writelines_to_file(reserved_file, reserved)
+    return reserved
 
 
 def parse_dhcp_settings(vlan_data):
     dhcp_settings = []
+    excluded = []
+    fixed = []
     vlan_name = vlan_data["name"]
     vlan_id = vlan_data["id"]
     mandatory = vlan_data["mandatoryDhcp"]
@@ -48,22 +50,27 @@ def parse_dhcp_settings(vlan_data):
     reserved_ips = vlan_data["reservedIpRanges"]
     fixed_ips = vlan_data["fixedIpAssignments"]
     if reserved_ips:
-        format_reserved_addresses(vlan_name, reserved_ips)
+        excluded = format_reserved_addresses(vlan_name, reserved_ips)
     if fixed_ips:
-        format_fixed_addresses(vlan_name, fixed_ips)
-
-
-    # Add settings to the list
-    dhcp_settings.append(f"VLAN {vlan_id}\n")
-    dhcp_settings.append(f"DHCP mandatory {mandatory}['enabled']\n")
-    dhcp_settings.append(f"Lease time {lease_time}\n")
-    dhcp_settings.append(f"DNS Servers {dns_servers}\n")
-    dhcp_settings.append(f"Boot options {boot_options}\n")
-    dhcp_settings.append(f"DHCP options {dhcp_options}\n")
-    dhcp_settings.append(f"Reserved IPs {reserved_ips}\n")
-    dhcp_settings.append(f"{reserved_ips}\n")
-    dhcp_settings.append(f"Fixed IPs {fixed_ips}\n")
-    fileops.writelines_to_file(vlan_name, dhcp_settings)
+        fixed = format_fixed_addresses(vlan_name, fixed_ips)
+    domain_name = "state.ut.us"
+    gateway = "1.1.1.1"
+    netmask = "255.255.255.0"
+    dhcp_settings.append("set dns-service default\n")
+    dhcp_settings.append(f"set domain {domain_name}\n")
+    dhcp_settings.append(f"set default-gateway {gateway}\n")
+    dhcp_settings.append(f"set netmask {netmask}\n")
+    dhcp_settings.append(f"set interface {vlan_name}\n")
+    dhcp_settings.append("config ip-range\n")
+    dhcp_settings.append("config ip-range\n")
+    dhcp_settings.append(f"    set start-ip {domain_name}\n")
+    dhcp_settings.append(f"    set end-ip {domain_name}\n")
+    dhcp_settings.append("next\n")
+    for exclude in excluded:
+        dhcp_settings.append(exclude)
+    for fix in fixed:
+        dhcp_settings.append(fix)
+    return dhcp_settings
 
 
 def main():
@@ -74,51 +81,53 @@ def main():
     org_id, org_name = merakiops.select_organization(dashboard)
     network = merakiops.select_network(dashboard, org_id)
 
-    # Check for warm spare. If found set serial number for both MXs
-    warm_spare = dashboard.appliance.getNetworkApplianceWarmSpare(network[0])
-    if warm_spare["enabled"]:
-        primary_sn = warm_spare["primarySerial"]
-        spare_sn = warm_spare["spareSerial"]
-        if warm_spare["uplinkMode"] == "virtual":
-            print(f"This site, {network[1]}, has a warm spare")
-            print(f"  VIP: {warm_spare['wan1']['ip']}")
-            print(f"  VIP Subnet: {warm_spare['wan1']['subnet']}")
-    # Else warm spare not found, set single MX serial number
-    else:
-        devices = dashboard.networks.getNetworkDevices(network[0])
-        # print(devices)
-        for device in devices:
-            if "MX" in device["model"]:
-                mx_sn = device["serial"]
+    ## Check for warm spare. If found set serial number for both MXs
+    #warm_spare = dashboard.appliance.getNetworkApplianceWarmSpare(network[0])
+    #if warm_spare["enabled"]:
+    #    primary_sn = warm_spare["primarySerial"]
+    #    spare_sn = warm_spare["spareSerial"]
+    #    if warm_spare["uplinkMode"] == "virtual":
+    #        print(f"This site, {network[1]}, has a warm spare")
+    #        print(f"  VIP: {warm_spare['wan1']['ip']}")
+    #        print(f"  VIP Subnet: {warm_spare['wan1']['subnet']}")
+    ## Else warm spare not found, set single MX serial number
+    #else:
+    #    devices = dashboard.networks.getNetworkDevices(network[0])
+    #    # print(devices)
+    #    for device in devices:
+    #        if "MX" in device["model"]:
+    #            mx_sn = device["serial"]
 
-    # For each MX found, print out WAN uplink settings
-    MXs = [primary_sn, spare_sn, mx_sn]
-    for each_MX in MXs:
-        if each_MX:
-            print("\nMX Serial Number: " + each_MX)
-            wan_info = dashboard.appliance.getDeviceApplianceUplinksSettings(each_MX)
-            # print(wan_info)
-            # Find enabled Uplink interfaces and extract config
-            for each in wan_info["interfaces"]:
-                if wan_info["interfaces"][each]["enabled"]:
-                    if (
-                        wan_info["interfaces"][each]["svis"]["ipv4"]["assignmentMode"]
-                        == "dynamic"
-                    ):
-                        print(f"  {each} Uplink is set to dynamic")
-                        break
-                    print(
-                        f"  {each} {wan_info['interfaces'][each]['svis']['ipv4']['address']}"
-                    )
-                    print(
-                        f"  {each} {wan_info['interfaces'][each]['svis']['ipv4']['gateway']}"
-                    )
-                    if wan_info["interfaces"][each]["vlanTagging"]["enabled"]:
-                        print(f"  {each} {wan_info['interfaces'][each]['vlanTagging']}")
-                        # print(wan_info['interfaces'][each]['vlanTagging']['vlanId'])
-            print()
+    ## For each MX found, print out WAN uplink settings
+    #MXs = [primary_sn, spare_sn, mx_sn]
+    #for each_MX in MXs:
+    #    if each_MX:
+    #        print("\nMX Serial Number: " + each_MX)
+    #        wan_info = dashboard.appliance.getDeviceApplianceUplinksSettings(each_MX)
+    #        # print(wan_info)
+    #        # Find enabled Uplink interfaces and extract config
+    #        for each in wan_info["interfaces"]:
+    #            if wan_info["interfaces"][each]["enabled"]:
+    #                if (
+    #                    wan_info["interfaces"][each]["svis"]["ipv4"]["assignmentMode"]
+    #                    == "dynamic"
+    #                ):
+    #                    print(f"  {each} Uplink is set to dynamic")
+    #                    break
+    #                print(
+    #                    f"  {each} {wan_info['interfaces'][each]['svis']['ipv4']['address']}"
+    #                )
+    #                print(
+    #                    f"  {each} {wan_info['interfaces'][each]['svis']['ipv4']['gateway']}"
+    #                )
+    #                if wan_info["interfaces"][each]["vlanTagging"]["enabled"]:
+    #                    print(f"  {each} {wan_info['interfaces'][each]['vlanTagging']}")
+    #                    # print(wan_info['interfaces'][each]['vlanTagging']['vlanId'])
+    #        print()
 
     # Find VLAN settings, handle case where vlans aren't enabled
+    dhcp_settings = []
+    output = []
     vlan_list = []
     if dashboard.appliance.getNetworkApplianceVlansSettings(network[0])["vlansEnabled"]:
         vlans = dashboard.appliance.getNetworkApplianceVlans(network[0])
@@ -135,11 +144,13 @@ def main():
             values = name + "," + str(vlan_id) + "," + subnet + "," + assigned_ip + "," + dhcp_handling + "," + group_policy + "\n"
             vlan_list.append(values)
             if "Run" in dhcp_handling:
-                parse_dhcp_settings(vlan)
+                fileops.append_to_file('dhcp.txt', parse_dhcp_settings(vlan))
     else:
         single_network = dashboard.appliance.getNetworkApplianceSingleLan(network[0])
         print("Vlans are not enabled")
         print(f"The single subnet is {single_network['subnet']}")
+    for each in dhcp_settings:
+        output.append(each)
 
     fileops.writelines_to_file('vlans.txt', vlan_list)
 
